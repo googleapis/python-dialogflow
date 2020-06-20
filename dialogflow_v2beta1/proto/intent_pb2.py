da5df6a1f7450e59e38030d379deb7c46714553c
yoshi-automation <yoshi-automation@google.com>
2020-06-20 06:47:01 -0700

    - Update grpc config for retry mechanism. - Update ImportAgent/ExportAgent external documentation.
    PiperOrigin-RevId: 317214667
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Jun 18 17:24:42 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 4bb127d2cfe943bd2813da3d66dce9e0b9ccb179
    Source-Link: https://github.com/googleapis/googleapis/commit/4bb127d2cfe943bd2813da3d66dce9e0b9ccb179

--- dialogflow_v2beta1/proto/intent_pb2.py
+++ dialogflow_v2beta1/proto/intent_pb2.py
@@ -6048,12 +6048,9 @@ Intent = _reflection.GeneratedProtocolMessageType(
                         __module__="google.cloud.dialogflow_v2beta1.proto.intent_pb2",
                         __doc__="""Carousel Rich Business Messaging (RBM) rich card.  Rich cards allow
       you to respond to users with more vivid content, e.g. with media and
-      suggestions.  For more details about RBM rich cards, please see:
-      https://developers.google.com/business-communications/rcs-business-
-      messaging/guides/build/messages/send#rich-cards If you want to show a
-      single card with more control over the layout, please use [RbmStandalo
-      neCard][google.cloud.dialogflow.v2beta1.Intent.Message.RbmStandaloneCa
-      rd] instead.
+      suggestions.  If you want to show a single card with more control over
+      the layout, please use [RbmStandaloneCard][google.cloud.dialogflow.v2b
+      eta1.Intent.Message.RbmStandaloneCard] instead.
       Attributes:
           card_width:
               Required. The width of the cards in the carousel.
@@ -6072,12 +6069,10 @@ Intent = _reflection.GeneratedProtocolMessageType(
                         __module__="google.cloud.dialogflow_v2beta1.proto.intent_pb2",
                         __doc__="""Standalone Rich Business Messaging (RBM) rich card.  Rich cards allow
       you to respond to users with more vivid content, e.g. with media and
-      suggestions.  For more details about RBM rich cards, please see:
-      https://developers.google.com/business-communications/rcs-business-
-      messaging/guides/build/messages/send#rich-cards You can group multiple
-      rich cards into one using [RbmCarouselCard][google.cloud.dialogflow.v2
-      beta1.Intent.Message.RbmCarouselCard] but carousel cards will give you
-      less control over the card layout.
+      suggestions.  You can group multiple rich cards into one using [RbmCar
+      ouselCard][google.cloud.dialogflow.v2beta1.Intent.Message.RbmCarouselC
+      ard] but carousel cards will give you less control over the card
+      layout.
       Attributes:
           card_orientation:
               Required. Orientation of the card.
@@ -6120,9 +6115,9 @@ Intent = _reflection.GeneratedProtocolMessageType(
                 download button.
             height:
                 Required for cards with vertical orientation. The height of
-                the media within a rich card with a vertical layout.
-                (https://goo.gl/NeFCjz). For a standalone card with horizontal
-                layout, height is not customizable, and this field is ignored.
+                the media within a rich card with a vertical layout. For a
+                standalone card with horizontal layout, height is not
+                customizable, and this field is ignored.
         """,
                                 # @@protoc_insertion_point(class_scope:google.cloud.dialogflow.v2beta1.Intent.Message.RbmCardContent.RbmMedia)
                             ),
@@ -6198,7 +6193,7 @@ Intent = _reflection.GeneratedProtocolMessageType(
                                 DESCRIPTOR=_INTENT_MESSAGE_RBMSUGGESTEDACTION_RBMSUGGESTEDACTIONDIAL,
                                 __module__="google.cloud.dialogflow_v2beta1.proto.intent_pb2",
                                 __doc__="""Opens the user's default dialer app with the specified phone number
-        but does not dial automatically (https://goo.gl/ergbB2).
+        but does not dial automatically.
         Attributes:
             phone_number:
                 Required. The phone number to fill in the default dialer app.
@@ -6215,10 +6210,10 @@ Intent = _reflection.GeneratedProtocolMessageType(
                             dict(
                                 DESCRIPTOR=_INTENT_MESSAGE_RBMSUGGESTEDACTION_RBMSUGGESTEDACTIONOPENURI,
                                 __module__="google.cloud.dialogflow_v2beta1.proto.intent_pb2",
-                                __doc__="""Opens the user's default web browser app to the specified uri
-        (https://goo.gl/6GLJD2). If the user has an app installed that is
-        registered as the default handler for the URL, then this app will be
-        opened instead, and its icon will be used in the suggested action UI.
+                                __doc__="""Opens the user's default web browser app to the specified uri If the
+        user has an app installed that is registered as the default handler
+        for the URL, then this app will be opened instead, and its icon will
+        be used in the suggested action UI.
         Attributes:
             uri:
                 Required. The uri to open on the user device
@@ -6233,7 +6228,7 @@ Intent = _reflection.GeneratedProtocolMessageType(
                                 DESCRIPTOR=_INTENT_MESSAGE_RBMSUGGESTEDACTION_RBMSUGGESTEDACTIONSHARELOCATION,
                                 __module__="google.cloud.dialogflow_v2beta1.proto.intent_pb2",
                                 __doc__="""Opens the device's location chooser so the user can pick a location to
-        send back to the agent (https://goo.gl/GXotJW).""",
+        send back to the agent.""",
                                 # @@protoc_insertion_point(class_scope:google.cloud.dialogflow.v2beta1.Intent.Message.RbmSuggestedAction.RbmSuggestedActionShareLocation)
                             ),
                         ),
