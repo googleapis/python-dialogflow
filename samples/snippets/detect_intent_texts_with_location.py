69bf02c733c7116840b15992f505cc298ed55b86
Ye Yuan <yuanye@google.com>
2021-02-12 14:25:33 -0800

    run blacken to reformat files and update remaining string formatting

--- samples/snippets/detect_intent_texts_with_location.py
+++ samples/snippets/detect_intent_texts_with_location.py
@@ -28,65 +28,70 @@ import uuid
 
 
 # [START dialogflow_detect_intent_text_with_location]
-def detect_intent_texts_with_location(project_id, location_id, session_id, texts, language_code):
+def detect_intent_texts_with_location(
+    project_id, location_id, session_id, texts, language_code
+):
     """Returns the result of detect intent with texts as inputs.
 
     Using the same `session_id` between requests allows continuation
     of the conversation."""
     from google.cloud import dialogflow
-    session_client = dialogflow.SessionsClient(client_options={
-       "api_endpoint": f"{location_id}-dialogflow.googleapis.com"})
 
-    session = f"projects/{project_id}/locations/{location_id}/agent/sessions/{session_id}"
-    print('Session path: {}\n'.format(session))
+    session_client = dialogflow.SessionsClient(
+        client_options={"api_endpoint": f"{location_id}-dialogflow.googleapis.com"}
+    )
+
+    session = (
+        f"projects/{project_id}/locations/{location_id}/agent/sessions/{session_id}"
+    )
+    print(f"Session path: {session}\n")
 
     for text in texts:
-        text_input = dialogflow.TextInput(
-            text=text, language_code=language_code)
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
 
         query_input = dialogflow.QueryInput(text=text_input)
 
         response = session_client.detect_intent(
-            request={'session': session, 'query_input': query_input})
+            request={"session": session, "query_input": query_input}
+        )
 
-        print('=' * 20)
-        print('Query text: {}'.format(response.query_result.query_text))
-        print('Detected intent: {} (confidence: {})\n'.format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence))
-        print('Fulfillment text: {}\n'.format(
-            response.query_result.fulfillment_text))
+        print("=" * 20)
+        print(f"Query text: {response.query_result.query_text}")
+        print(
+            f"Detected intent: {response.query_result.intent.display_name} (confidence: {response.query_result.intent_detection_confidence,})\n"
+        )
+        print(f"Fulfillment text: {response.query_result.fulfillment_text}\n")
+
+
 # [END dialogflow_detect_intent_text_with_location]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
     parser.add_argument(
-        '--project-id',
-        help='Project/agent id.  Required.',
-        required=True)
+        "--project-id", help="Project/agent id.  Required.", required=True
+    )
+    parser.add_argument("--location-id", help="Location id.  Required.", required=True)
     parser.add_argument(
-        '--location-id',
-        help='Location id.  Required.',
-        required=True)
+        "--session-id",
+        help="Identifier of the DetectIntent session. " "Defaults to a random UUID.",
+        default=str(uuid.uuid4()),
+    )
     parser.add_argument(
-        '--session-id',
-        help='Identifier of the DetectIntent session. '
-        'Defaults to a random UUID.',
-        default=str(uuid.uuid4()))
-    parser.add_argument(
-        '--language-code',
+        "--language-code",
         help='Language code of the query. Defaults to "en-US".',
-        default='en-US')
-    parser.add_argument(
-        'texts',
-        nargs='+',
-        type=str,
-        help='Text inputs.')
+        default="en-US",
+    )
+    parser.add_argument("texts", nargs="+", type=str, help="Text inputs.")
 
     args = parser.parse_args()
 
     detect_intent_texts_with_location(
-        args.project_id, args.location_id, args.session_id, args.texts, args.language_code)
+        args.project_id,
+        args.location_id,
+        args.session_id,
+        args.texts,
+        args.language_code,
+    )
--- samples/snippets/detect_intent_texts_with_location_test.py
+++ samples/snippets/detect_intent_texts_with_location_test.py
@@ -18,15 +18,26 @@ import uuid
 
 from detect_intent_texts_with_location import detect_intent_texts_with_location
 
-PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
-LOCATION_ID = 'europe-west2'
-SESSION_ID = 'test_{}'.format(uuid.uuid4())
-TEXTS = ["hello", "book a meeting room", "Mountain View",
-         "tomorrow", "10 AM", "2 hours", "10 people", "A", "yes"]
+PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
+LOCATION_ID = "europe-west2"
+SESSION_ID = "test_{}".format(uuid.uuid4())
+TEXTS = [
+    "hello",
+    "book a meeting room",
+    "Mountain View",
+    "tomorrow",
+    "10 AM",
+    "2 hours",
+    "10 people",
+    "A",
+    "yes",
+]
 
 
 def test_detect_intent_texts_with_location(capsys):
-    detect_intent_texts_with_location(PROJECT_ID, LOCATION_ID, SESSION_ID, TEXTS, 'en-GB')
+    detect_intent_texts_with_location(
+        PROJECT_ID, LOCATION_ID, SESSION_ID, TEXTS, "en-GB"
+    )
     out, _ = capsys.readouterr()
 
-    assert 'Fulfillment text: All set!' in out
+    assert "Fulfillment text: All set!" in out
