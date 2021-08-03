4952c45bbbae0e4a326f45d6d00e407ae2b0441f
Gal Zahavi <galzahavi@google.com>
2021-08-03 10:00:02 -0700

    Fixed Lint Issues

--- samples/snippets/update_intent_test.py
+++ samples/snippets/update_intent_test.py
@@ -1,17 +1,19 @@
 import os
-import unittest
 from datetime import date
+import unittest
+
 from google.cloud.dialogflow_v2.services.agents.client import AgentsClient
-from google.cloud.dialogflow_v2.services.intents.client import IntentsClient
 from google.cloud.dialogflow_v2 import Agent
+from google.cloud.dialogflow_v2.services.intents.client import IntentsClient
 from google.cloud.dialogflow_v2.types.agent import (
     DeleteAgentRequest,
     SearchAgentsRequest,
 )
 
-from update_intent import updateIntent
+from update_intent import update_intent
 
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
+
 
 def create_agent(project_id, display_name):
     parent = "projects/" + project_id
@@ -23,7 +25,7 @@ def create_agent(project_id, display_name):
         time_zone="America/Los_Angeles",
     )
 
-    response = agents_client.set_agent(request={"agent": agent})
+    agents_client.set_agent(request={"agent": agent})
 
 
 def list_agent(self, project_id):
@@ -73,7 +75,7 @@ class fieldmaskTest(unittest.TestCase):
             print("Created Intent in setUp")
 
     def test_update_intent(self):
-        actualResponse = updateIntent(
+        actualResponse = update_intent(
             PROJECT_ID or "", self.intent_id, "Updated Intent"
         )
         expectedResponse = "Updated Intent"
