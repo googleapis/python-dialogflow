293e65fa042e6be163c00a5c3b4b5a36a17ae655
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2021-08-31 16:55:57 -0700

    Update update_intent.py

--- samples/snippets/update_intent.py
+++ samples/snippets/update_intent.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # [START dialogflow_es_update_intent]
 
 from google.cloud.dialogflow_v2 import IntentsClient
