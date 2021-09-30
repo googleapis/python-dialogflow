3a3e1dfa54b5ebd48f35cee20aba8423eff90cd5
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2021-09-30 10:20:38 -0700

    Added comment

--- samples/snippets/webhook.py
+++ samples/snippets/webhook.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# returns fullfillment response for dialogflow detect_intent call
 
 # [START dialogflow_webhook]
 def handleWebhook(request):
