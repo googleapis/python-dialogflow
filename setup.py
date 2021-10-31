3eeb18873efc20914ce40bb70b23c3e4345f902d
Anthonios Partheniou <partheniou@google.com>
2021-10-31 10:35:51 +0000

    fix(deps): drop packaging dependency

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ dependencies = [
     # https://github.com/googleapis/google-cloud-python/issues/10566
     "google-api-core[grpc] >= 1.28.0, <3.0.0dev",
     "proto-plus >= 1.10.0",
-    "packaging >= 14.3",
 ]
 
 package_root = os.path.abspath(os.path.dirname(__file__))
